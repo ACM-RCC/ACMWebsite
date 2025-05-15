5398a6a08233dbb6486631a3d1e0c598d1967725
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2025-05-15 10:47:30 -0700

    Temp placeholders and text

--- acmwebsite/src/pages/index.tsx
+++ acmwebsite/src/pages/index.tsx
@@ -26,20 +26,18 @@ export default function Home() {
 				/>
 				<ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
 					<li className="mb-2 tracking-[-.01em]">
-						Get started by editing{" "}
+						yo{" "}
 						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-							src/pages/index.tsx
+							momma
 						</code>
 						.
 					</li>
-					<li className="tracking-[-.01em]">
-						Save and see your changes instantly.
-					</li>
+					<li className="tracking-[-.01em]">syed smells.</li>
 				</ol>
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<a
-						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-orange-600 dark:hover:bg-orange-600 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+						href="https://discord.gg/WPennuNF"
 						target="_blank"
 						rel="noopener noreferrer">
 						<Image
